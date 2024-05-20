7c2cf0c84aa7e8394df7ed8158b4c3ef4f6d289f
Nikola P. <nikoladevelops@gmail.com>
2024-05-20 19:11:55 +0300

    Created MainFrame

---
+++ LibraryApp/LibraryApp/src/MainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class MainFrame extends JFrame {
+    public MainFrame(){
+        this.setTitle("LibraryApp");
+        this.setSize(500,500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
